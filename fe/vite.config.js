4397a4f9f868fe5f87a3b3396212de710a0678ac
Nguyen Vu Duy Hoai <mynamehoaidz@gmail.com>
2025-07-07 10:59:30 +0700

    Fix: add base path for Vite to support Vercel routing

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./", // <-- thêm dòng này để tránh lỗi sai đường dẫn khi deploy
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3001",
-    },
-  },
+  server: { proxy: { "/api": "http://localhost:3001" } },
 });
