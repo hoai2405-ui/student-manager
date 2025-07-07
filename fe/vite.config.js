b78117250a01e16b6e8ecabf462c134a5fa6bebe
Nguyen Vu Duy Hoai <mynamehoaidz@gmail.com>
2025-07-07 15:00:54 +0700

    fix: update Vercel config and vite config

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // ⚠️ PHẢI CÓ
-  plugins: [react()]
+  base: "./", // 👈 Đây là quan trọng nhất!
+  build: {
+    outDir: "dist", // mặc định, giữ nguyên nếu bạn chưa sửa
+  },
+  plugins: [react()],
   // server: { proxy: { "/api": "http://localhost:3001" } },
 });
